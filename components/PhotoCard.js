f0c48701d9cad02d63b3deb5a019a264f3de6aef
蓓蕾 朱 <beileizhu@MacBook-ProBeilei.local>
2024-02-05 17:25:27 -0600

    fix trash

--- components/PhotoCard.js
+++ components/PhotoCard.js
@@ -39,7 +39,7 @@ function PhotoCard({ photoObj, onUpdate }) {
             )}
             {photoObj.type}
           </Col>
-          <Col sm={3} style={{ marginTop: '-1.2rem', marginRight: '-1rem' }}>
+          <Col sm={3} style={{ marginTop: '-2.2rem', marginRight: '-0.5rem' }}>
             {photoObj.uid === user.uid ? (
               <div
                 role="button" // Adding role for accessibility
@@ -47,8 +47,10 @@ function PhotoCard({ photoObj, onUpdate }) {
                 onClick={deleteThisPhoto}
                 onKeyDown={handleKeyDown} // Handling keyboard events
                 className="mt-3 black-button-svg"
+                style={{ cursor: 'pointer', padding: '1rem' }}
+
               >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" style={{ height: '20px', width: '20px' }}>
                   {/* Font Awesome SVG path */}
                   <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
                 </svg>
